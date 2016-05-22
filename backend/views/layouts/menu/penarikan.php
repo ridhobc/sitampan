dc3ae57247b4be37b1d441c069bedb2fe53479eb
avrilia08 <ridhobc@gmail.com>
2016-05-22 07:21:05 +0700

    perubahan pada menu penarikan bcf15

--- backend/views/layouts/menu/penarikan.php
+++ backend/views/layouts/menu/penarikan.php
@@ -4,8 +4,9 @@
 <?php
 $bcfbaru = \backend\models\Bcf15::find()->where(['status_bcf15' => ['3']])->count();
 $bcterima = backend\models\SkepPenetapanBcf15::find()->where(['status_skep' => ['konsep']])->count();
-$tpp = \backend\models\Bcf15Detail::find()->where(['tpp_id' => ['1']])->count();
+$tpp = \backend\models\Bcf15Detail::find()->where(['tpp_id' => ['1'],'status_bcf15_detail' => ['2']])->count();
 $srtpmdh = \backend\models\Bcf15SuratPemindahan::find()->where(['status_surat' => ['konsep']])->count();
+$stilltps = \backend\models\Bcf15Detail::find()->where(['masuk_tpp' => ['0'],'status_bcf15_detail' => ['2']])->count();
 ?>
 <li class="treeview active">
     <?= Html::a('<i class="fa fa-truck"></i> <span>' . ('Penarikan') . '</span> <i class="fa fa-angle-left pull-right"></i>', ['/default/index']) ?>
@@ -23,7 +24,7 @@ $srtpmdh = \backend\models\Bcf15SuratPemindahan::find()->where(['status_surat' =
             <?= Html::a('<i class="fa fa-angle-double-right"></i> ' . Yii::t('app', 'Surat Pemindahan <span class="label label-warning"> ' . ($srtpmdh) . ' </span> '), ['/penarikan/surat-pemindahan/index']) ?>
         </li>
         <li>		
-            <?= Html::a('<i class="fa fa-angle-double-right"></i> ' . Yii::t('app', 'Masih di TPS <span class="label label-warning"> ' . ($srtpmdh) . ' </span> '), ['/penarikan/surat-pemindahan/still-in-tps']) ?>
+            <?= Html::a('<i class="fa fa-angle-double-right"></i> ' . Yii::t('app', 'Masih di TPS <span class="label label-warning"> ' . ($stilltps) . ' </span> '), ['/penarikan/surat-pemindahan/still-in-tps']) ?>
         </li>
         
         <li>
